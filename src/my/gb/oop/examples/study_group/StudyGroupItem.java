880aae6dc3ecc96c81bdd1ada741c8548e7e0ffe
Zhanna <zh_23@list.ru>
2024-06-09 21:28:16 +0300

    реализована структура mvp/ версия 3.0 (убраны файлы из другого проекта)

--- src/my/gb/oop/examples/study_group/StudyGroupItem.java
+++
@@ -1,8 +0,0 @@
-package my.gb.oop.examples.study_group;
-
-import java.time.LocalDate;
-
-public interface StudyGroupItem <T> extends Comparable<T>{
-    LocalDate getBirthDate();
-    String getName();
-}
